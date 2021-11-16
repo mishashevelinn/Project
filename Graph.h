57c77a1a107e13b72bd4f50502875ee7388dbe06
Michael Shevelin <shevel55505@gmail.com>
2021-11-16 08:58:59 +0200

    branching

--- Graph.h
+++ Graph.h
@@ -5,4 +5,45 @@
 #ifndef PROJECT_GRAPH_H
 #define PROJECT_GRAPH_H
 
+#include <bits/stdc++.h>
+using namespace std;
+
+enum Color {
+    WHITE,
+    GRAY,
+    BLACK,
+};
+
+string getString(Color color) {
+    switch (color) {
+        case WHITE:
+            return "WHITE";
+        case GRAY:
+            return "GRAY";
+        case BLACK:
+            return "BLACK";
+    }
+    throw;
+}
+
+class Vertex {
+public:
+    const string name;
+    Color color;
+    int d = 0;
+    int f = 0;
+    int cc;
+    Vertex* PI;
+    Vertex() : color(WHITE), d(INT_MAX), PI(nullptr) {
+    }
+    Vertex(const string& name) : name(name) {
+    }
+};
+
+class Graph {
+public:
+    vector<Vertex*> V;
+    map<Vertex*, vector<Vertex*>> Adj;
+};
+
 #endif //PROJECT_GRAPH_H
