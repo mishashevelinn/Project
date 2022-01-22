f8efb960aa631abc605cace0619b0485bd6e4774
Tals8623 <tals5shamir@gmail.com>
2022-01-22 20:58:18 +0200

    fixed some bugs, now its working without writing data

--- main.cpp
+++ main.cpp
@@ -15,40 +15,48 @@ using namespace io;
 //}
 
 int main() {
-    int max_g = 15;
+    int g = 5;
+    int n = 20;
     double success = 0;
     double failure = 0;
-    double iteration = 100;
-    double avgTime = 0;
-    double avgAvailVertex = 0;
-    vector<int> N;
-    N = { 1000,500,400,300, 200, 100, 70, 50, 30, 20, 15, 10};
-    ofstream file("res2.txt");
-    ofstream fileGraph("graph.txt");
-    clock_t start, end;
-    for (int g = 5; g < max_g; g++) {
-        for (int n: N) {
-            for (int i = 0; i < iteration; i++) {
-                Graph G(n);
-                start = clock();
-                if (solve(G, g)) {
-                    success++;
-                } else {
-                    failure++;
-                }
-                avgAvailVertex += G.availVertexes.size();
-                end = clock();
-                avgTime += double(end - start) / double(CLOCKS_PER_SEC);
+//    double iteration = 1;
+//    double avgTime = 0;
+//    double avgAvailVertex = 0;
 
-            }
-            avgTime /= iteration;
-            io::write_stats(file, n, g, double(success / iteration), avgTime, avgAvailVertex / failure);
-            success = 0;
-            failure = 0;
-            avgTime = 0;
-            avgAvailVertex = 0;
-        }
+//    vector<int> N;
+//    N = { 1000,500,400,300, 200, 100, 70, 50, 30, 20, 15, 10};
+//    ofstream file("res2.txt");
+//    ofstream fileGraph("graph.txt");
+//    clock_t start, end;
+//    for (int g = 5; g < max_g; g++) {
+//        for (int n: N) {
+//            for (int i = 0; i < iteration; i++) {
+    for (int k=0; k<5; k++){
+        Graph G(n, g);
+//    start = clock();
+        cout << solve(G, g);
     }
+
+
+    //    if (solve(G, g)) {
+//        success++;
+//    } else {
+//        failure++;
+//    }
+//                avgAvailVertex += G.availVertexes.size();
+//    end = clock();
+//    avgTime += double(end - start) / double(CLOCKS_PER_SEC);
+
+//            }
+//            avgTime /= iteration;
+//            cout << "Success: " << success << ",tries: " << iteration << endl;
+//            io::write_stats(file, n, g, double(success / iteration), avgTime);
+//            success = 0;
+//            failure = 0;
+//            avgTime = 0;
+//            avgAvailVertex = 0;
+//        }
+//    }
     return 0;
 }
 
