c415be8c98fbec8a3957c661a58d2ec6b6c71765
tal1995 <tal5shamir@gmail.com>
2022-05-14 14:03:56 +0300

    Add algorithm B implementation and one output graph "testGraph" to check validation (Graph is 4K/ e)

--- Algos.h
+++ Algos.h
@@ -16,20 +16,15 @@ namespace tools {
         SINGLE_CYCLE = 1,
     } newCirclesOnG;
 
-    void clear(Graph &G, vector<int> d, char which = 'v') {
-        if (which == 'd') {
-            for (int v = 0; v < G.n; v++) {
-                d[v] = 0;
-            }
-        } else if (which == 'v') {
-            for (int v = 0; v < G.n; v++) {
-                G.visited[v] = false;
-                G.PI[v] = -1;
-            }
+    void clear(Graph &G) {
+        for (int v = 0; v < G.n; v++) {
+            G.d[v] = 0;
+            G.visited[v] = false;
+            G.PI[v] = -1;
         }
     }
 
-
+    
     int random(int min, int max) {
         std::random_device rd;     // only used once to initialise (seed) engine
         std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
@@ -109,7 +104,7 @@ namespace tools {
 //        assert(G.isNeighbour(edgeToRemove.first, edgeToRemove.second) == -1);
 
         ///DEBUG: after removal of random edge on the cycle the condition should be always FALSE///
-//        tools::clear(G, G.d1);
+//        tools::clear(G);
 //        int cycles = tools::count_cycles(G, u, v, G.g-2);
 //        if (cycles == 1){
 //            if(G.isNeighbour(u,v) != -1){
@@ -156,6 +151,67 @@ namespace tools {
         cout << endl;
     }
 
+
+    bool generateEdge(Graph &g, int v) {
+        vector<int> availVertex;
+        for (int i = 0; i < g.legalDeg.size(); i++){
+            if (isLegalNeighbour(g, g.legalDeg[i])){
+                availVertex.push_back(g.legalDeg[i]);
+            }
+        }
+        if (availVertex.empty()) return false;
+        int u = random(0, availVertex.size() - 1);
+        g.connect(v, availVertex[u]);
+        return true;
+    }
+
+
+    void bfs(Graph &G, int root, int g) {
+        tools::clear(G);
+        queue<int> Q;
+        Q.push(root);
+        G.visited[root] = true;
+        G.visited_track.push_back(root);
+        while (!Q.empty()) {
+            int v = Q.front();
+            Q.pop();
+            for (int u : G.Adj[v]) {
+                if (!G.visited[u]) {
+                    G.d[u] = G.d[v] + 1;
+                    G.visited[u] = true;
+                    G.visited_track.push_back(u);
+                    if (G.d[u] == g-1) return;
+                    Q.push(u);
+                }
+            }
+        }
+    }
+    
+    
+    vector<int> getRandOrder(vector<int> &vertexes) {
+        // copy all even vertexes to new order vector and shuffle them, to random permutation
+        vector<int> order(vertexes);  
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::default_random_engine e(seed);
+        std::shuffle(order.begin(), order.end(), e);
+        return order;
+    }
+
+
+    bool solve_B(Graph &G, int g) {
+        vector<int> random_vertex_order = getRandOrder(G.legalDeg);
+        for (int v: random_vertex_order){
+            if (G.Adj[v].size() != 3) {
+                bfs(G, v, g);
+                if (!tools::generateEdge(G, v)) return false;
+                G.visited[v] = false;
+            }
+        }
+        return true;
+
+    }
+
+
     bool solve(Graph &G, int g, int max_iter) {
         int iter = 0;
         int TALSHA_replaceCounter = 0;
@@ -175,7 +231,7 @@ namespace tools {
                     G.connect(u, v);
                     break;
                 case SINGLE_CYCLE: {  // (u,v) is 1-candidate
-                    tools::clear(G, G.d1);
+                    tools::clear(G);
                     G.findPath(u, v);  // shortest path closing the single cycle with E+(u,v),
                     list<int> route = G.trace_route(u, v);
 //                    io::print_route(route,u,v);
